c93f5eb3cc29ecda389d8d55100e8808a3562587
Amanda Waite <amandarwaite@gmail.com>
2024-03-21 14:32:27 +0000

    More tutorials

---
+++ group_chat_sequential.py
@@ -0,0 +1,78 @@
+import os
+import json
+from autogen import ConversableAgent, GroupChat, GroupChatManager
+
+# The Number Agent always returns the same numbers.
+number_agent = ConversableAgent(
+    name="Number_Agent",
+    system_message="You return me the numbers I give you, one number each line.",
+    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+    human_input_mode="NEVER",
+)
+
+# The Adder Agent adds 1 to each number it receives.
+adder_agent = ConversableAgent(
+    name="Adder_Agent",
+    system_message="You add 1 to each number I give you and return me the new numbers, one number each line.",
+    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+    human_input_mode="NEVER",
+)
+
+# The Multiplier Agent multiplies each number it receives by 2.
+multiplier_agent = ConversableAgent(
+    name="Multiplier_Agent",
+    system_message="You multiply each number I give you by 2 and return me the new numbers, one number each line.",
+    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+    human_input_mode="NEVER",
+)
+
+# The Subtracter Agent subtracts 1 from each number it receives.
+subtracter_agent = ConversableAgent(
+    name="Subtracter_Agent",
+    system_message="You subtract 1 from each number I give you and return me the new numbers, one number each line.",
+    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+    human_input_mode="NEVER",
+)
+
+# The Divider Agent divides each number it receives by 2.
+divider_agent = ConversableAgent(
+    name="Divider_Agent",
+    system_message="You divide each number I give you by 2 and return me the new numbers, one number each line.",
+    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+    human_input_mode="NEVER",
+)
+
+# The `description` attribute is a string that describes the agent.
+# It can also be set in `ConversableAgent` constructor.
+adder_agent.description = "I am an adder agent, I can only add 1 to each input number."
+multiplier_agent.description = "I am a multiplier agent. I can only multiply each input number by 2."
+subtracter_agent.description = "I am a subtractor agent. I can only subtract 1 from each input number."
+divider_agent.description = "I am a divider agent. I can only divide each input number by 2."
+number_agent.description = "Return the numbers given."
+
+group_chat_with_intros = GroupChat(
+    agents=[number_agent, adder_agent, multiplier_agent, subtracter_agent, divider_agent],
+    max_round=10,
+    messages=[],
+    send_introductions=True,
+)
+
+group_chat_manager_with_intros = GroupChatManager(
+    groupchat=group_chat_with_intros,
+    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+)
+
+# Start a sequence of two-agent chats between the number agent and
+# the group chat manager.
+chat_result = number_agent.initiate_chats(
+    [
+        {
+            "recipient": group_chat_manager_with_intros,
+            "message": "My number is 3, I want to turn it into 13.",
+        },
+        {
+            "recipient": group_chat_manager_with_intros,
+            "message": "Turn this number into 32.",
+        },
+    ]
+)
